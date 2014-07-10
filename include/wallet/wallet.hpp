6f408b853b2753562a21ff6d6ecf1acfe6866c83
evoskuil <eric@voskuil.org>
2014-07-09 23:57:58 -0600

    Exclude deprecated electrum_keys from VS builds and include.

--- include/wallet/wallet.hpp
+++ include/wallet/wallet.hpp
@@ -22,7 +22,7 @@
 // Convenience header that includes everything
 // Not to be used internally. For API users.
 #include <wallet/define.hpp>
-#include <wallet/electrum_keys.hpp>
+//#include <wallet/electrum_keys.hpp>
 #include <wallet/hd_keys.hpp>
 #include <wallet/key_formats.hpp>
 #include <wallet/mnemonic.hpp>
