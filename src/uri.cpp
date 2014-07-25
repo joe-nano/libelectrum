17a00116b1347e1aecfb5d6c691f3ab9ef00e77d
William Swanson <swansontec@gmail.com>
2014-07-25 14:50:09 -0700

    Move amount formatting/parsing to its own file

--- src/uri.cpp
+++ src/uri.cpp
@@ -17,10 +17,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include <boost/algorithm/string.hpp>
 #include <bitcoin/bitcoin.hpp>
 #include <wallet/define.hpp>
 #include <wallet/uri.hpp>
+#include <wallet/amount.hpp>
 
 namespace libwallet {
 
@@ -162,60 +162,6 @@ BCW_API bool uri_parse_result::got_param(std::string& key, std::string& value)
     else if (!key.compare(0, 4, "req-"))
         return false;
     return true;
-}
-
-BCW_API uint64_t parse_amount(const std::string& amount,
-    unsigned decmial_places)
-{
-    auto i = amount.begin();
-    uint64_t value = 0;
-    unsigned places = 0;
-
-    while (amount.end() != i && is_digit(*i))
-    {
-        value = 10*value + (*i - '0');
-        ++i;
-    }
-    if (amount.end() != i && '.' == *i)
-    {
-        ++i;
-        while (amount.end() != i && is_digit(*i))
-        {
-            if (places < decmial_places)
-                value = 10*value + (*i - '0');
-            else if (places == decmial_places && '5' <= *i)
-                value += 1;
-            ++places;
-            ++i;
-        }
-    }
-    while (places < decmial_places)
-    {
-        value *= 10;
-        ++places;
-    }
-    if (amount.end() != i)
-        return invalid_amount;
-    return value;
-}
-
-BCW_API std::string format_amount(uint64_t amount, unsigned decimal_places)
-{
-    // Get the integer and fractional parts:
-    uint64_t factor = 1;
-    for (unsigned i = 0; i < decimal_places; ++i)
-        factor *= 10;
-    uint64_t int_part = amount / factor;
-    uint64_t decimal_part = amount % factor;
-    // Format as a fixed-point number:
-    std::ostringstream stream;
-    stream << int_part << '.';
-    stream << std::setw(decimal_places) << std::setfill('0') << decimal_part;
-    // Trim trailing zeros:
-    auto string = stream.str();
-    boost::algorithm::trim_right_if(string, [](char c){ return '0' == c; });
-    boost::algorithm::trim_right_if(string, [](char c){ return '.' == c; });
-    return string;
 }
 
 /**
